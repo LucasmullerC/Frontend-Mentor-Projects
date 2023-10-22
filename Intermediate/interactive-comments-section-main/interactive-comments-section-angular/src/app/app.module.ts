17732e93a029abad93b798ac89aeb61e07af48c2
Lucas Muller <lucas.mullerqdv@gmail.com>
2023-10-22 11:00:38 -0300

    Implemented main container, created comment-item component

--- Intermediate/interactive-comments-section-main/interactive-comments-section-angular/src/app/app.module.ts
+++ Intermediate/interactive-comments-section-main/interactive-comments-section-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommentItemComponent } from './comment-item/comment-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentItemComponent
   ],
   imports: [
     BrowserModule
